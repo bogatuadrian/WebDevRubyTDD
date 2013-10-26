6e932b4ba335be625298bd789b867a9a22f1e9b2
Adrian Bogatu <bogatu.adrian@gmail.com>
2013-10-26 13:29:57 +0300

    Game of Life implementation passing all tests

--- CGOL/cgol.rb
+++ CGOL/cgol.rb
@@ -1,9 +1,105 @@
 require 'set'
 
 class Cell < Struct.new(:x, :y)
+  def +(cell)
+    Cell.new(self[:x] + cell[:x], self[:y] + cell[:y])
+  end
 end
 
+class Directions
+  def Directions::getDirections()
+    directions = Array.new
+    (-1..1).each do |i|
+      (-1..1).each do |j|
+        directions << Cell.new(i, j) unless i == 0 && j == 0
+      end
+    end
+    directions
+  end
+end
+
+def get_neighbours(cell)
+  directions = Directions::getDirections
+
+  res = Array.new
+
+  directions.each do |d|
+    neighbour = cell + d
+    res << neighbour if (yield neighbour)
+  end
+
+  res
+end
+
+=begin
+def alive_neighbours(cell, alive_cells)
+  directions = Directions::getDirections
+
+  res = Array.new
+
+  directions.each do |d|
+    neighbour = cell + d
+    res << neighbour if alive_cells[neighbour] == true
+  end
+
+  res
+end
+=end
+
+def direct_surroundings_of(alive_cells)
+  neighbours = Array.new
+  alive_cells.keys.each do |cell|
+    neighbours = neighbours + (get_neighbours(cell) { |k| alive_cells[k] != true })
+  end
+
+  neighbours
+end
+
+def dead_cells_that_become_alive(alive_cells)
+  dead_cells = direct_surroundings_of(alive_cells)
+  new_alive_cells = Array.new
+
+  dead_cells.each do |cell|
+    neighbours = get_neighbours(cell) { |k| alive_cells[k] == true }
+
+    new_alive_cells << cell if neighbours.count == 3
+  end
+
+  new_alive_cells
+end
+
+def alive_cells_that_stay_alive(alive_cells)
+
+  new_alive_cells = Array.new
+
+  alive_cells.each do |cell, v|
+    neighbours = get_neighbours(cell) { |k| alive_cells[k] == true }
+
+    new_alive_cells << cell if neighbours.count == 2 || neighbours.count == 3
+  end
+
+  new_alive_cells
+end
+
+
 def evolve_universe(seed)
-  [].to_set
+
+  return [].to_set if seed == [].to_set
+
+  alive_cells = Hash.new
+
+  seed.each do |e|
+    alive_cells[e] = true
+  end
+
+
+=begin
+  res = Array.new
+  new_alive_cells.each do |k, v|
+    res << k if new_alive_cells[k] == true
+  end
+=end
+  res = alive_cells_that_stay_alive(alive_cells) + dead_cells_that_become_alive(alive_cells)
+  res.to_set
 end
 
--- CGOL/testsuite.rb
+++ CGOL/testsuite.rb
@@ -3,7 +3,6 @@ require './cgol.rb'
 
 class TestConwaysGameOfLife < Test::Unit::TestCase
 
-=begin
   def test_with_an_empty_seed
     # Arrange (a.k.a. define input)
     seed = [].to_set
@@ -19,9 +18,7 @@ class TestConwaysGameOfLife < Test::Unit::TestCase
       failure_message = "An empty seed does not evolve into an empty seed as expected"
     )
   end # def test_with_an_empty_seed
-=end
 
-=begin
   def test_with_a_seed_of_00
     # Arrange (a.k.a. define input)
     seed = [Cell.new(0,0)].to_set
@@ -37,9 +34,7 @@ class TestConwaysGameOfLife < Test::Unit::TestCase
       failure_message = "A seed of [(0,0)] does not evolve into an empty seed as expected"
     )
   end # def test_with_a_seed_of_00
-=end
 
-=begin
   def test_with_a_seed_of_00_01
     # Arrange (a.k.a. define input)
     seed =
@@ -58,9 +53,7 @@ class TestConwaysGameOfLife < Test::Unit::TestCase
       failure_message = "A seed of [(0,0),(0,1)] does not evolve into an empty seed as expected"
     )
   end # def test_with_a_seed_of_00_01
-=end
 
-=begin
   def test_with_a_seed_of_00_01_10
     # Arrange (a.k.a. define input)
     seed =
@@ -89,9 +82,7 @@ class TestConwaysGameOfLife < Test::Unit::TestCase
       failure_message = "A seed of #{s} does not evolve into #{e} as expected"
     )
   end # def test_with_a_seed_of_00_01_10
-=end
 
-=begin
   def test_with_a_seed_of_10_21_02_12_22
     # Arrange (a.k.a. define input)
     seed =
@@ -123,9 +114,7 @@ class TestConwaysGameOfLife < Test::Unit::TestCase
       failure_message = "A seed of #{s} does not evolve into #{e} as expected"
     )
   end # def test_with_a_seed_of_10_21_02_12_22
-=end
 
-=begin
   def test_with_a_seed_of_01_12_13_21_22
     # Arrange (a.k.a. define input)
     seed =
@@ -157,7 +146,6 @@ class TestConwaysGameOfLife < Test::Unit::TestCase
       failure_message = "A seed of #{s} does not evolve into #{e} as expected"
     )
   end # def test_with_a_seed_of_01_12_13_21_22
-=end
 
 end
 
