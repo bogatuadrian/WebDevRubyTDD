ed1ab988e9394cfc72c28b0efda61d59c793c43e
Adrian Bogatu <bogatu.adrian@gmail.com>
2013-10-26 11:30:13 +0300

    Added the newest source for Pascals Triangle

--- PascalsTriangle/pascalstriangle.rb
+++ PascalsTriangle/pascalstriangle.rb
@@ -1,4 +1,27 @@
+
 def compute_row(n)
-  []
+  return [] if n < 1
+  return [1] if n == 1
+
+  curr = []
+  n.times do
+    result = []
+    curr.inject(0) do |acc, x|
+      result << (x + acc)
+      x
+    end
+    result << 1
+    curr = result
+  end
+
+  curr
 end
 
+__END__
+
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1
+1 5 10 10 5 1
